b34d444f6f114f48a4869f35f7d5ce154444ba66
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2024-09-08 22:56:55 -0700

    Update sales.cpp
    remove syntax error

--- sales.cpp
+++ sales.cpp
@@ -10,10 +10,10 @@ int main(){
   int countyTax = totalSales * countyTaxRate;
   int totalTax = stateTax + countyTax;
 
-  std::cout << "You have earned $" + totalSales + " in total" << std::endl;
-  std::cout << "You owe $" + stateTax + " in state tax" << std::endl;
-  std::cout << "You owe $" + countyTax + " in county tax" << std::endl;
-  std::cout << "In total, you owe $" + totalTax + "in tax" << std::endl;
+  std::cout << "You have earned $" << totalSales << " in total" << std::endl;
+  std::cout << "You owe $" << stateTax << " in state tax" << std::endl;
+  std::cout << "You owe $" << countyTax << " in county tax" << std::endl;
+  std::cout << "In total, you owe $" << totalTax << "in tax" << std::endl;
 
   return 0;
 
