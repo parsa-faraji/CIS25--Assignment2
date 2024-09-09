ba9f8baa3b4c6b018bc67bbf0a48f82b35ed51d9
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2024-09-08 22:53:54 -0700

    Update sales.cpp
    calculate taxes

--- sales.cpp
+++ sales.cpp
@@ -3,5 +3,10 @@
 
 int main(){
   int totalSales = 95000;
-  const double stateTax = 0.04;
-  const double countyTax = 0.02;
+  const double stateTaxRate = 0.04;
+  const double countyTaxRate = 0.02;
+
+  int stateTax = totalSales * stateTaxRate;
+  int countyTax = totalSales * countyTaxRate;
+  int totalTax = stateTax + countyTax;
+
