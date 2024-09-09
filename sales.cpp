bd5d311f779b3fe0039e98cafc16eaff24b1ed76
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2024-09-08 22:51:50 -0700

    Create sales.cpp
    define variables

---
+++ sales.cpp
@@ -0,0 +1,7 @@
+// This program calculates and displays total tax, state tax, and county tax for a specifc year
+#include <iostream>
+
+int main(){
+  int totalSales = 95000;
+  const double stateTax = 0.04;
+  const double countyTax = 0.02;
